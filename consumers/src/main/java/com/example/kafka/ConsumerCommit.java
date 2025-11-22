a917739b1e928d14af67775de13a0d0ff06cadd8
fad <ojy9406@naver.com>
2025-11-22 15:24:31 +0900

    feat: Kafka Consumer의 비동기 커밋 기능 테스트

--- consumers/src/main/java/com/example/kafka/ConsumerCommit.java
+++ consumers/src/main/java/com/example/kafka/ConsumerCommit.java
@@ -5,6 +5,9 @@ import org.apache.kafka.clients.consumer.ConsumerConfig;
 import org.apache.kafka.clients.consumer.ConsumerRecord;
 import org.apache.kafka.clients.consumer.ConsumerRecords;
 import org.apache.kafka.clients.consumer.KafkaConsumer;
+import org.apache.kafka.clients.consumer.OffsetAndMetadata;
+import org.apache.kafka.clients.consumer.OffsetCommitCallback;
+import org.apache.kafka.common.TopicPartition;
 import org.apache.kafka.common.errors.WakeupException;
 import org.apache.kafka.common.serialization.StringDeserializer;
 import org.slf4j.Logger;
@@ -12,6 +15,7 @@ import org.slf4j.LoggerFactory;
 
 import java.time.Duration;
 import java.util.List;
+import java.util.Map;
 import java.util.Properties;
 
 public class ConsumerCommit {
@@ -39,7 +43,8 @@ public class ConsumerCommit {
             }
         }));
 
-        pollCommitSync(kafkaConsumer);
+//        pollCommitSync(kafkaConsumer);
+        pollCommitASync(kafkaConsumer);
     }
 
     private static void pollAutoCommit(KafkaConsumer<String, String> kafkaConsumer) {
@@ -97,6 +102,48 @@ public class ConsumerCommit {
             logger.error("Exception", e);
         }
         finally {
+            kafkaConsumer.close();
+            logger.info("============ finally Kafka Consumer closed ============");
+        }
+    }
+
+    private static void pollCommitASync(KafkaConsumer<String, String> kafkaConsumer) {
+        int loopCount = 0;
+
+        try {
+            while (true) {
+                ConsumerRecords<String, String> consumerRecords = kafkaConsumer.poll(Duration.ofMillis(1000));
+                logger.info("####### loopCount: {} consumerRecord count:{}, loopCount++", loopCount++, consumerRecords.count());
+
+                for (ConsumerRecord<String, String> record : consumerRecords) {
+                    logger.info("offset : {} | key : {} | partition : {} | value : {}",
+                            record.offset(), record.key(), record.partition(), record.value());
+                }
+                kafkaConsumer.commitAsync(new OffsetCommitCallback() {
+                    @Override
+                    public void onComplete(Map<TopicPartition, OffsetAndMetadata> offsets, Exception exception) {
+                        if (exception != null) {
+                            logger.error("Commit failed for offsets: {}", offsets, exception);
+                        } else {
+                            logger.info("Commit succeeded for offsets: {}", offsets);
+                        }
+                    }
+                });
+            }
+        } catch (WakeupException e) {
+            logger.error("Wakeup Exception", e);
+        } catch (Exception e) {
+            logger.error("Exception", e);
+        }
+        finally {
+            try {
+                logger.info("============ finally Kafka Consumer Commit ============");
+            } catch (CommitFailedException e) {
+                logger.error("Commit Failed Exception during final commitSync", e);
+            }
+            finally {
+                kafkaConsumer.commitSync();
+            }
             kafkaConsumer.close();
             logger.info("============ finally Kafka Consumer closed ============");
         }
